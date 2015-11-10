321f2345c0bf94c72f86eb05219e3aebd25c2869
iquery <1406422800@qq.com>
2015-11-10 17:58:38 +0800

    还有两片处问题没有修改

--- src/js/main.js
+++ src/js/main.js
@@ -1,225 +1,343 @@
-;(function($, window, undefined) {
+;
+(function(window) {
 
   'use strict';
 
-  $.DropDown = function(options, element) {
-    this.$el = $(element);
-    this._init(options);
+  /**
+   * based on from https://github.com/inuyaksa/jquery.nicescroll/blob/master/jquery.nicescroll.js
+   */
+  function hasParent(e, p) {
+    if (!e) return false;
+    var el = e.target || e.srcElement || e || false;
+    while (el && el != p) {
+      el = el.parentNode || false;
+    }
+    return (el !== false);
   };
 
-  // the options
-  $.DropDown.defaults = {
-    speed: 300,
-    easing: 'ease',
-    gutter: 0,
-    // initial stack effect
-    stack: true,
-    // delay between each option animation
-    delay: 0,
-    // random angle and positions for the options
-    random: false,
-    // rotated [right||left||false] : the options will be rotated to thr right side or left side.
-    // make sure to tune the transform origin in the stylesheet
-    rotated: false,
-    // effect to slide in the options. value is the margin to start with
-    slidingIn: false,
-    onOptionSelect: function(opt) {
-      return false;
+  /**
+   * extend obj function
+   */
+  function extend(a, b) {
+    for (var key in b) {
+      if (b.hasOwnProperty(key)) {
+        a[key] = b[key];
+      }
     }
-  };
-
-  $.DropDown.prototype = {
-
-    _init: function(options) {
-
-      // options
-      this.options = $.extend(true, {}, $.DropDown.defaults, options);
-      this._layout();
-      this._initEvents();
-
-    },
-    _layout: function() {
-
-      var self = this;
-      this.minZIndex = 1000;
-      var value = this._transformSelect();
-      this.opts = this.listopts.children('li');
-      this.optsCount = this.opts.length;
-      this.size = {
-        width: this.dd.width(),
-        height: this.dd.height()
-      };
-
-      var elName = this.$el.attr('name'),
-        elId = this.$el.attr('id'),
-        inputName = elName !== undefined ? elName : elId !== undefined ? elId : 'cd-dropdown-' + (new Date()).getTime();
-
-      this.inputEl = $('<input type="hidden" name="' + inputName + '" value="' + value + '"></input>').insertAfter(this.selectlabel);
-
-      this.selectlabel.css('z-index', this.minZIndex + this.optsCount);
-      this._positionOpts();
-      if (Modernizr.csstransitions) {
-        setTimeout(function() {
-          self.opts.css('transition', 'all ' + self.options.speed + 'ms ' + self.options.easing);
-        }, 25);
-      }
-
-    },
-    _transformSelect: function() {
-
-      var optshtml = '',
-        selectlabel = '',
-        value = -1;
-      this.$el.children('option').each(function() {
-
-        var $this = $(this),
-          val = isNaN($this.attr('value')) ? $this.attr('value') : Number($this.attr('value')),
-          classes = $this.attr('class'),
-          selected = $this.attr('selected'),
-          label = $this.text();
-
-        if (val !== -1) {
-          optshtml +=
-            classes !== undefined ?
-            '<li data-value="' + val + '"><span class="' + classes + '">' + label + '</span></li>' :
-            '<li data-value="' + val + '"><span>' + label + '</span></li>';
-        }
-
-        if (selected) {
-          selectlabel = label;
-          value = val;
-        }
-
-      });
-
-      this.listopts = $('<ul/>').append(optshtml);
-      this.selectlabel = $('<span/>').append(selectlabel);
-      this.dd = $('<div class="cd-dropdown"/>').append(this.selectlabel, this.listopts).insertAfter(this.$el);
-      this.$el.remove();
-
-      return value;
-
-    },
-    _positionOpts: function(anim) {
-
-      var self = this;
-
-      this.listopts.css('height', 'auto');
-      this.opts
-        .each(function(i) {
-          $(this).css({
-            zIndex: self.minZIndex + self.optsCount - 1 - i,
-            top: self.options.slidingIn ? (i + 1) * (self.size.height + self.options.gutter) : 0,
-            left: 0,
-            marginLeft: self.options.slidingIn ? i % 2 === 0 ? self.options.slidingIn : -self.options.slidingIn : 0,
-            opacity: self.options.slidingIn ? 0 : 1,
-            transform: 'none'
-          });
-        });
-
-      if (!this.options.slidingIn) {
-        this.opts
-          .eq(this.optsCount - 1)
-          .css({
-            top: this.options.stack ? 9 : 0,
-            left: this.options.stack ? 4 : 0,
-            width: this.options.stack ? this.size.width - 8 : this.size.width,
-            transform: 'none'
-          })
-          .end()
-          .eq(this.optsCount - 2)
-          .css({
-            top: this.options.stack ? 6 : 0,
-            left: this.options.stack ? 2 : 0,
-            width: this.options.stack ? this.size.width - 4 : this.size.width,
-            transform: 'none'
-          })
-          .end()
-          .eq(this.optsCount - 3)
-          .css({
-            top: this.options.stack ? 3 : 0,
-            left: 0,
-            transform: 'none'
-          });
-      }
-
-    },
-    _initEvents: function() {
-
-      var self = this;
-
-      this.selectlabel.on('mousedown.dropdown', function(event) {
-        self.opened ? self.close() : self.open();
-        return false;
-
-      });
-
-      this.opts.on('click.dropdown', function() {
-        if (self.opened) {
-          var opt = $(this);
-          self.options.onOptionSelect(opt);
-          self.inputEl.val(opt.data('value'));
-          self.selectlabel.html(opt.html());
-          self.close();
-        }
-      });
-
-    },
-    open: function() {
-      var self = this;
-      this.dd.toggleClass('cd-active');
-      this.listopts.css('height', (this.optsCount + 1) * (this.size.height + this.options.gutter));
-      this.opts.each(function(i) {
-
-        $(this).css({
-          opacity: 1,
-          top: self.options.rotated ? self.size.height + self.options.gutter : (i + 1) * (self.size.height + self.options.gutter),
-          left: self.options.random ? Math.floor(Math.random() * 11 - 5) : 0,
-          width: self.size.width,
-          marginLeft: 0,
-          transform: self.options.random ?
-            'rotate(' + Math.floor(Math.random() * 11 - 5) + 'deg)' : self.options.rotated ?
-            self.options.rotated === 'right' ?
-            'rotate(-' + (i * 5) + 'deg)' :
-            'rotate(' + (i * 5) + 'deg)' : 'none',
-          transitionDelay: self.options.delay && Modernizr.csstransitions ? self.options.slidingIn ? (i * self.options.delay) + 'ms' : ((self.optsCount - 1 - i) * self.options.delay) + 'ms' : 0
-        });
-
-      });
-      this.opened = true;
-
-    },
-    close: function() {
-
-      var self = this;
-      this.dd.toggleClass('cd-active');
-      if (this.options.delay && Modernizr.csstransitions) {
-        this.opts.each(function(i) {
-          $(this).css({
-            'transition-delay': self.options.slidingIn ? ((self.optsCount - 1 - i) * self.options.delay) + 'ms' : (i * self.options.delay) + 'ms'
-          });
-        });
-      }
-      this._positionOpts(true);
-      this.opened = false;
-
-    }
-
+    return a;
   }
 
-  $.fn.dropdown = function(options) {
-    var instance = $.data(this, 'dropdown');
-    if (typeof options === 'string') {
-      var args = Array.prototype.slice.call(arguments, 1);
-      this.each(function() {
-        instance[options].apply(instance, args);
-      });
-    } else {
-      this.each(function() {
-        instance ? instance._init() : instance = $.data(this, 'dropdown', new $.DropDown(options, this));
-      });
-    }
-    return instance;
-  };
+  /**
+   * SelectFx function
+   */
+  function SelectFx(el, options) {
+    this.el = el;
+    this.options = extend({}, this.options);
+    extend(this.options, options);
+    this._init();
+  }
 
-})(jQuery, window);
+  /**
+   * SelectFx options
+   */
+  SelectFx.prototype.options = {
+    // if true all the links will open in a new tab.
+    // if we want to be redirected when we click an option, we need to define a data-link attr on the option of the native select element
+    newTab: true,
+    // when opening the select element, the default placeholder (if any) is shown
+    stickyPlaceholder: true,
+    // callback when changing the value
+    onChange: function(val) {
+      return false;
+    }
+  }
+
+  /**
+   * init function
+   * initialize and cache some vars
+   */
+  SelectFx.prototype._init = function() {
+    // check if we are using a placeholder for the native select box
+    // we assume the placeholder is disabled and selected by default
+    var selectedOpt = this.el.querySelector('option[selected]');
+    this.hasDefaultPlaceholder = selectedOpt && selectedOpt.disabled;
+
+    // get selected option (either the first option with attr selected or just the first option)
+    this.selectedOpt = selectedOpt || this.el.querySelector('option');
+
+    // create structure
+    this._createSelectEl();
+
+    // all options
+    this.selOpts = [].slice.call(this.selEl.querySelectorAll('li[data-option]'));
+
+    // total options
+    this.selOptsCount = this.selOpts.length;
+
+    // current index
+    this.current = this.selOpts.indexOf(this.selEl.querySelector('li.cs-selected')) || -1;
+
+    // placeholder elem
+    this.selPlaceholder = this.selEl.querySelector('span.cs-placeholder');
+
+    // init events
+    this._initEvents();
+  }
+
+  /**
+   * creates the structure for the select element
+   */
+  SelectFx.prototype._createSelectEl = function() {
+    var self = this,
+      options = '',
+      createOptionHTML = function(el) {
+        var optclass = '',
+          classes = '',
+          link = '';
+
+        if (el.selectedOpt && !this.foundSelected && !this.hasDefaultPlaceholder) {
+          classes += 'cs-selected ';
+          this.foundSelected = true;
+        }
+        // extra classes
+        if (el.getAttribute('data-class')) {
+          classes += el.getAttribute('data-class');
+        }
+        // link options
+        if (el.getAttribute('data-link')) {
+          link = 'data-link=' + el.getAttribute('data-link');
+        }
+
+        if (classes !== '') {
+          optclass = 'class="' + classes + '" ';
+        }
+
+        return '<li ' + optclass + link + ' data-option data-value="' + el.value + '"><span>' + el.textContent + '</span></li>';
+      };
+
+    [].slice.call(this.el.children).forEach(function(el) {
+      if (el.disabled) {
+        return;
+      }
+
+      var tag = el.tagName.toLowerCase();
+
+      if (tag === 'option') {
+        options += createOptionHTML(el);
+      } else if (tag === 'optgroup') {
+        options += '<li class="cs-optgroup"><span>' + el.label + '</span><ul>';
+        [].slice.call(el.children).forEach(function(opt) {
+          options += createOptionHTML(opt);
+        })
+        options += '</ul></li>';
+      }
+    });
+
+    var opts_el = '<div class="cs-options"><ul>' + options + '</ul></div>';
+    this.selEl = document.createElement('div');
+    this.selEl.className = this.el.className;
+    this.selEl.tabIndex = this.el.tabIndex;
+    this.selEl.innerHTML = '<span class="cs-placeholder">' + this.selectedOpt.textContent + '</span>' + opts_el;
+    this.el.parentNode.innerHTML(this.selEl);
+    this.selEl.innerHTML(this.el);
+    console.log( this.el );
+  }
+
+
+  /**
+   * initialize the events
+   */
+  SelectFx.prototype._initEvents = function() {
+    var self = this;
+
+    // open/close select
+    this.selPlaceholder.addEventListener('click', function() {
+      self._toggleSelect();
+    });
+
+    // clicking the options
+    this.selOpts.forEach(function(opt, idx) {
+      opt.addEventListener('click', function() {
+        self.current = idx;
+        self._changeOption();
+        // close select elem
+        self._toggleSelect();
+      });
+    });
+
+    // close the select element if the target it´s not the select element or one of its descendants..
+    document.addEventListener('click', function(ev) {
+      var target = ev.target;
+      if (self._isOpen() && target !== self.selEl && !hasParent(target, self.selEl)) {
+        self._toggleSelect();
+      }
+    });
+
+    // keyboard navigation events
+    // this.selEl.addEventListener('keydown', function(ev) {
+    //   var keyCode = ev.keyCode || ev.which;
+
+    //   switch (keyCode) {
+    //     // up key
+    //     case 38:
+    //       ev.preventDefault();
+    //       self._navigateOpts('prev');
+    //       break;
+    //       // down key
+    //     case 40:
+    //       ev.preventDefault();
+    //       self._navigateOpts('next');
+    //       break;
+    //       // space key
+    //     case 32:
+    //       ev.preventDefault();
+    //       if (self._isOpen() && typeof self.preSelCurrent != 'undefined' && self.preSelCurrent !== -1) {
+    //         self._changeOption();
+    //       }
+    //       self._toggleSelect();
+    //       break;
+    //       // enter key
+    //     case 13:
+    //       ev.preventDefault();
+    //       if (self._isOpen() && typeof self.preSelCurrent != 'undefined' && self.preSelCurrent !== -1) {
+    //         self._changeOption();
+    //         self._toggleSelect();
+    //       }
+    //       break;
+    //       // esc key
+    //     case 27:
+    //       ev.preventDefault();
+    //       if (self._isOpen()) {
+    //         self._toggleSelect();
+    //       }
+    //       break;
+    //   }
+    // });
+  }
+
+  /**
+   * navigate with up/dpwn keys
+   */
+  SelectFx.prototype._navigateOpts = function(dir) {
+    if (!this._isOpen()) {
+      this._toggleSelect();
+    }
+
+    var tmpcurrent = typeof this.preSelCurrent != 'undefined' && this.preSelCurrent !== -1 ? this.preSelCurrent : this.current;
+
+    if (dir === 'prev' && tmpcurrent > 0 || dir === 'next' && tmpcurrent < this.selOptsCount - 1) {
+      // save pre selected current - if we click on option, or press enter, or press space this is going to be the index of the current option
+      this.preSelCurrent = dir === 'next' ? tmpcurrent + 1 : tmpcurrent - 1;
+      // remove focus class if any..
+      this._removeFocus();
+      // add class focus - track which option we are navigating
+      classie.add(this.selOpts[this.preSelCurrent], 'cs-focus');
+    }
+  }
+
+  /**
+   * open/close select
+   * when opened show the default placeholder if any
+   */
+  SelectFx.prototype._toggleSelect = function() {
+    // remove focus class if any..
+    this._removeFocus();
+    // console.log(this.selPlaceholder.textContent )
+    var txtparent = $(this.selPlaceholder).parent().parent();
+    if (this._isOpen()) {
+      if (this.current !== -1) {
+        // update placeholder text
+        this.selPlaceholder.textContent = this.selOpts[this.current].textContent;
+        if (this.selPlaceholder.textContent == "单选") {
+          $(txtparent).find(".single").show();
+          $(txtparent).find(".fill").hide();
+        } else if (this.selPlaceholder.textContent == "填空") {
+          $(txtparent).find(".fill").show();
+          $(txtparent).find(".single").hide();
+        } else if (this.selPlaceholder.textContent == "文档") {
+          $(txtparent).find(".file").show();
+          $(txtparent).find(".txt").hide();
+        } else if (this.selPlaceholder.textContent == "文字") {
+          $(txtparent).find(".txt").show();
+          $(txtparent).find(".file").hide();
+        }
+      }
+      classie.remove(this.selEl, 'cs-active');
+    } else {
+      if (this.hasDefaultPlaceholder && this.options.stickyPlaceholder) {
+        // everytime we open we wanna see the default placeholder text
+        this.selPlaceholder.textContent = this.selectedOpt.textContent;
+      }
+      classie.add(this.selEl, 'cs-active');
+    }
+  }
+
+  /**
+   * change option - the new value is set
+   */
+  SelectFx.prototype._changeOption = function() {
+    // if pre selected current (if we navigate with the keyboard)...
+    if (typeof this.preSelCurrent != 'undefined' && this.preSelCurrent !== -1) {
+      this.current = this.preSelCurrent;
+      this.preSelCurrent = -1;
+    }
+
+    // current option
+    var opt = this.selOpts[this.current];
+
+    // update current selected value
+    this.selPlaceholder.textContent = opt.textContent;
+
+    // change native select element´s value
+    this.el.value = opt.getAttribute('data-value');
+
+    // remove class cs-selected from old selected option and add it to current selected option
+    var oldOpt = this.selEl.querySelector('li.cs-selected');
+    if (oldOpt) {
+      classie.remove(oldOpt, 'cs-selected');
+    }
+    classie.add(opt, 'cs-selected');
+
+    // if there´s a link defined
+    if (opt.getAttribute('data-link')) {
+      // open in new tab?
+      if (this.options.newTab) {
+        window.open(opt.getAttribute('data-link'), '_blank');
+      } else {
+        window.location = opt.getAttribute('data-link');
+      }
+    }
+
+    // callback
+    this.options.onChange(this.el.value);
+  }
+
+  /**
+   * returns true if select element is opened
+   */
+  SelectFx.prototype._isOpen = function(opt) {
+    return classie.has(this.selEl, 'cs-active');
+  }
+
+  /**
+   * removes the focus class from the option
+   */
+  SelectFx.prototype._removeFocus = function(opt) {
+    var focusEl = this.selEl.querySelector('li.cs-focus')
+    if (focusEl) {
+      classie.remove(focusEl, 'cs-focus');
+    }
+  }
+
+  /**
+   * add to global namespace
+   */
+  window.SelectFx = SelectFx;
+
+})(window);
+
+(function() {
+  [].slice.call(document.querySelectorAll('select.cs-select')).forEach(function(el) {
+    new SelectFx(el);
+  });
+})();
