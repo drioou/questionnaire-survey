ce337d8d3a5b169cdbeef02ce3e6f0d3968b69cd
iquery <1406422800@qq.com>
2015-12-16 17:56:59 +0800

    2015年12月16日 17:56:57

--- js/1216.js
+++ js/1216.js
@@ -2,7 +2,7 @@
  * @Author: duanyong
  * @Date:   2015-12-16 17:05:54
  * @Last Modified by:   duanyong
- * @Last Modified time: 2015-12-16 17:30:41
+ * @Last Modified time: 2015-12-16 17:54:21
  */
 var support = {
         transitions: Modernizr.csstransitions
@@ -31,3 +31,41 @@ var support = {
             onEndCallbackFn();
         }
     },
+    shzEl = document.querySelector('.component'),
+    shzCtrl = shzEl.querySelector('button.button--start'),
+    shzSVGEl = shzEl.querySelector('svg.morpher'),
+    snap = Snap(shzSVGEl),
+    totalNotes = 50,
+    notes,
+    notesSpeedFactor = 4.5,
+    simulateTime = 6500,
+    winsize = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    },
+    shzCtrlSize = {
+        width: shzCtrl.offsetWidth,
+        height: shzCtrl.offsetHeight
+    },
+    isListening = false,
+    playerEl = shzEl.querySelector('.player'),
+    playerCloseCtrl = playerEl.querySelector('.button--close');
+function init(){
+  createNotes();
+  initEvents();
+}
+function createNotes(){
+  var noteEl = document.createElement('div'),notesElContent = '';
+  notesEl.className = 'notes';
+  for(var i = 0; i < totalNotes;++i){
+    var j =(i+1)-6*Math.floor(i/6);
+    notesElContent += "<div class="note icon icon--note' + j +' "><div> ';
+  }
+  noteEl.innerHTML = noteElContent;
+  shzEl.inserttBefore(notesEl,shzEl.firstChild)
+  notes = [].slice.call(notesEl.querySelectorAll('.note'));
+}
+function initEvents(){
+  shzCtrl.addeventListener('click',listen);
+  playerCloseCtrl.addeventListener('click',closePlayer);
+}
